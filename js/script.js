0c8c86de5ec8a43d4f4c0ba152eb7d863b0ff505
m.petrenko <mandarinkashusha@gmail.com>
2023-06-13 09:48:46 +0300

    цикли while

--- js/script.js
+++ js/script.js
@@ -5,10 +5,22 @@
 while (num < 5){
     console.log(num);
     num++;
-}*/
+}
 
 let num = 0;
 for (; num < 5; num++) {
     if(num == 2) continue;
     console.log(num);
+}
+*/
+
+//Метки
+firstFor: 
+for(let num = 0; num < 2; num++){
+    for (let size = 0; size < 3; size++) {
+        if (size == 2){
+            continue firstFor;
+        }
+        console.log(size);
+    }
 }
