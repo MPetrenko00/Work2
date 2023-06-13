6045ac10871152866d173935b2bf939c3d4780bd
m.petrenko <mandarinkashusha@gmail.com>
2023-06-13 09:39:58 +0300

    continue

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Цикл While
 
 //Приклад
-let num = 0;
+/*let num = 0;
 while (num < 5){
     console.log(num);
     num++;
+}*/
+
+let num = 0;
+for (; num < 5; num++) {
+    if(num == 2) continue;
+    console.log(num);
 }
